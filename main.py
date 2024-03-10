511b9bb596b98b379401f720bbb2d9e4d75efa35
Marwollo.Modire <marwollo.modire@gmail.com>
2024-03-10 10:07:42 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# Этот бот отправляет по любому запросу фото котиков!
+import requests
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_URL = 'https://api.telegram.org/bot'
+API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+ERROR_TEXT = 'Эх, а обещали ведь котика показать... Плак!'
+
+offset = -2
+counter = 0
+cat_response: requests.Response
+cat_link: str
+
+
+while counter < 100:
+    print('Котик номер : ', counter)
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            cat_response = requests.get(API_CATS_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]['url']
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+            else:
+                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+
+    time.sleep(1)
+    counter += 1
